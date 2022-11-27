ea6dc1ab26b65b4654a97780517f7c5063d77a6e
Liliya Slugina <lili3345@yandex.ru>
2022-11-27 15:24:01 +0300

    Спринт 6

---
+++ src/test/java/FelineWithoutMock.java
@@ -0,0 +1,43 @@
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+public class FelineWithoutMock {
+    private Feline feline;
+
+    @Before
+    public void createNewObj() {
+        feline = new Feline();
+    }
+
+    @Test
+    public void eatMeat() throws Exception {
+        List<String> list = feline.eatMeat();
+        int actual = list.size();
+        int expected = 3;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFamily() {
+        String actual = feline.getFamily();
+        String expected = "Кошачьи";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getKittens() {
+        int actual = feline.getKittens();
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+}
