35c2352573eb80741dfc42019dd95736fe55d5ae
Liliya Slugina <lili3345@yandex.ru>
2022-12-01 10:09:05 +0300

    fixed class

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,51 +1,44 @@
-import com.example.Feline;
-import org.junit.Assert;
+package com.example;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 
-@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Mock
-    Feline feline;
+    private Feline feline;
 
-    @Test
-    public void retrieveGetFamily() {
-        feline.getFamily();
-        Mockito.verify(feline, Mockito.times(1)).getFamily();
-        Mockito.when(feline.getFamily()).thenReturn("Кошачьи");
-        String expected = "Кошачьи";
-        Assert.assertEquals(expected, feline.getFamily());
+    @Before
+    public void createNewObj() {
+        feline = new Feline();
     }
 
     @Test
-    public void retrieveGetKittens() {
-        feline.getKittens(1);
-        Mockito.verify(feline).getKittens(Mockito.anyInt());
-        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
-        Mockito.when(feline.getKittens(1)).thenReturn(1);
+    public void retrieveGetFamily() {
+        String actual = feline.getFamily();
+        String expected = "Кошачьи";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getKittens() {
+        int actual = feline.getKittens();
         int expected = 1;
-        Assert.assertEquals(expected, feline.getKittens(1));
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void eatMeat() throws Exception {
-        feline.eatMeat();
-        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+        List<String> list = feline.eatMeat();
+        int actual = list.size();
+        int expected = 3;
+        assertEquals(expected, actual);
     }
 
 
-    @Test
-    public void retrieveGetKittensWithoutArg() {
-        feline.getKittens();
-        Mockito.verify(feline, Mockito.times(1)).getKittens();
-        Mockito.when(feline.getKittens()).thenReturn(1);
-        int expected = 1;
-        Assert.assertEquals(expected, feline.getKittens());
-
-    }
 }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,27 +1,39 @@
-import com.example.Feline;
-import com.example.Lion;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+package com.example;
 
 import java.util.List;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertTrue;
 
 
-
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+    Lion lion;
     private static final String validGender = "Самец";
+    private static final String invalidGender = "Undefined";
+    private static final String exceptionMessage = "Используйте допустимые значения пола животного - самей или самка";
     @Mock
     private Feline feline;
 
+    @Rule
+    public final ExpectedException except = ExpectedException.none();
+
+    @Before
+    public void createNewObj() throws Exception {
+        lion = new Lion(validGender, feline);
+    }
+
     @Test
     public void getKittens() throws Exception {
-        Lion lion = new Lion(validGender, feline);
+
         Mockito.when(feline.getKittens()).thenReturn(1);
         int actual = lion.getKittens();
         int expected = 1;
@@ -31,7 +43,7 @@ public class LionTest {
 
     @Test
     public void doesHaveMane() throws Exception {
-        Lion lion = new Lion(validGender, feline);
+
         boolean actual = lion.doesHaveMane();
 
         assertTrue(actual);
@@ -39,7 +51,7 @@ public class LionTest {
 
     @Test
     public void getFood() throws Exception {
-        Lion lion = new Lion(validGender, feline);
+
         Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> actual = lion.getFood();
         List<String> expected = List.of("Животные", "Птицы", "Рыба");
@@ -47,5 +59,21 @@ public class LionTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void exceptNullPointerException() {
+        except.expect(NullPointerException.class);
+        throw new NullPointerException();
+    }
 
+    @Test
+    public void expectExceptionMessage() throws Exception  {
+        except.expectMessage(exceptionMessage);
+        Lion lion = new Lion(invalidGender,feline);
+
+
+    }
 }
+
+
+
+
